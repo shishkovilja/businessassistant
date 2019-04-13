2cabb7e4dd85892cbd3a310e4397630c33c6ddc2
Ilya Shishkov <shishkovilja@yandex.ru>
2019-04-14 01:20:56 +0300

    feature/BA-0046: Added JSP support: project folders structure changed, ServletInitializer used, spring-starter-tomcat artifact added int general pom.xml, war packaging used for rest module

--- rest/src/main/java/io/khasang/ba/config/application/SpringBootWebApp.java
+++ rest/src/main/java/io/khasang/ba/config/application/SpringBootWebApp.java
@@ -3,11 +3,12 @@ package io.khasang.ba.config.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 //TODO dog_nail-1 => Do we need EntityManager?
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
-public class WebApp {
+public class SpringBootWebApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(WebApp.class, args);
+        SpringApplication.run(SpringBootWebApp.class, args);
     }
 }
