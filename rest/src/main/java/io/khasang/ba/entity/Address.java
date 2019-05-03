a2bb6d18527523c942999bcade4f4240333b727c
Mozgachev Ivan <mozgachev.ivan@gmail.com>
2019-05-03 07:08:23 +0300

    added methods getter/setter by id

--- rest/src/main/java/io/khasang/ba/entity/Address.java
+++ rest/src/main/java/io/khasang/ba/entity/Address.java
@@ -62,6 +62,14 @@ public class Address {
         this.hause = hause;
     }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getRegion() {
         return region;
     }
