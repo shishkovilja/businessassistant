c2f635de87f44abab07c45f1adbdfb8fb84f55b1
Ilya Shishkov <shishkovilja@yandex.ru>
2019-05-09 23:29:19 +0300

    enhancement/ba-0092: Customer entity and CustomerInformation changed with Lombok annotations, getters, setters, equals and hashCode methods deleted

--- rest/src/main/java/io/khasang/ba/entity/Customer.java
+++ rest/src/main/java/io/khasang/ba/entity/Customer.java
@@ -1,21 +1,27 @@
 package io.khasang.ba.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.NaturalId;
 import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 /**
  * Customer entity class. Contains embeddable class {@link CustomerInformation}
  */
 @Entity
 @Table(name = "customers")
+@Setter
+@Getter
+@EqualsAndHashCode
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @EqualsAndHashCode.Exclude
     private Long id;
 
     @NotBlank
@@ -23,6 +29,7 @@ public class Customer {
     private String login;
 
     @Column(name = "registration_date", columnDefinition = "TIMESTAMP")
+    @EqualsAndHashCode.Exclude
     private LocalDateTime registrationTimestamp;
 
     @NotBlank
@@ -34,68 +41,4 @@ public class Customer {
 
     @Embedded
     private CustomerInformation customerInformation;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public LocalDateTime getRegistrationTimestamp() {
-        return registrationTimestamp;
-    }
-
-    public void setRegistrationTimestamp(LocalDateTime registrationTimestamp) {
-        this.registrationTimestamp = registrationTimestamp;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public CustomerInformation getCustomerInformation() {
-        return customerInformation;
-    }
-
-    public void setCustomerInformation(CustomerInformation customerInformation) {
-        this.customerInformation = customerInformation;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(login, customer.login) &&
-                Objects.equals(password, customer.password) &&
-                Objects.equals(email, customer.email) &&
-                Objects.equals(customerInformation, customer.customerInformation);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(login, password, email, customerInformation);
-    }
 }
