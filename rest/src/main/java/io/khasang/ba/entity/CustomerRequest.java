c429b501e2fdbeca711f26f6ef0115a63d07966d
Ilya Shishkov <shishkovilja@yandex.ru>
2019-05-26 12:23:17 +0300

    feature/ba-0052: JavaDoc fix in CustomerRequest

--- rest/src/main/java/io/khasang/ba/entity/CustomerRequest.java
+++ rest/src/main/java/io/khasang/ba/entity/CustomerRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 
 /**
- * This domain entity class is purposed to provide possibility to each customer to submit requests for some type of service
+ * This domain entity class is purposed to provide possibility for each customer to submit requests for some type of service
  */
 @Data
 @Entity
